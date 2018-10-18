d6bd139d8a45d78d0865fe257cffce59e9995240
LilJohny <ivanenko@ucu.edu.ua>
2018-10-18 09:28:17 +0300

    Added Ulam numbers generator

--- numbers_generators.py
+++ numbers_generators.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Generator():
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -5,5 +8,30 @@ class Generator():
     def get_sequence(self, length):
         pass
 
-    def get_random_number(self):
+    def get_random_number(self, max_number_of_sequnce):
         pass
+
+
+class UlamNumbersGenerator(Generator):
+    def __init__(self):
+        self.sequence = [1, 2, 3]
+
+    def get_sequence(self, length):
+        first = 0
+        second = 1
+
+        for i in range(length - 2):
+            if i % 2 == 0:
+                self.sequence.append(
+                    self.sequence[first] + self.sequence[second + 1])
+                second += 1
+            else:
+                self.sequence.append(
+                    self.sequence[first + 1] + self.sequence[second + 1])
+                first += 1
+                second += 1
+        return self.sequence
+
+    def get_random_number(self, max_number_of_sequnce):
+        ulam_sequence = self.get_sequence(max_number_of_sequnce)
+        return random.choice(ulam_sequence)
