60c886e8a33115c21913a4c25bbeeba71475faa6
LilJohny <ivanenko@ucu.edu.ua>
2018-10-18 08:58:54 +0300

    Added abstract class for numbers generators

---
+++ numbers_generators.py
@@ -0,0 +1,9 @@
+class Generator():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_sequence(self, length):
+        pass
+
+    def get_random_number(self):
+        pass
